f117396a112b76c451b7fb56122711ab79a140d9
yashua-ovando <yashua.ovando@geminos.ai>
2021-05-26 10:29:08 -0700

    integrated parser: nodes to typeql

--- Gruntfile.js
+++ Gruntfile.js
@@ -179,6 +179,7 @@ module.exports = function(grunt) {
                     "packages/node_modules/@node-red/editor-client/src/js/ui/editor.js",
                     "packages/node_modules/@node-red/editor-client/src/js/ui/editors/*.js",
                     "packages/node_modules/@node-red/editor-client/src/js/ui/event-log.js",
+                    "packages/node_modules/@node-red/editor-client/src/js/ui/typeql.js",
                     "packages/node_modules/@node-red/editor-client/src/js/ui/tray.js",
                     "packages/node_modules/@node-red/editor-client/src/js/ui/clipboard.js",
                     "packages/node_modules/@node-red/editor-client/src/js/ui/library.js",
