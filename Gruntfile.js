814f659be1d110da321b618b460320cee441a51a
Yashua Ovando <yashuaovando@Yashuas-MBP.home>
2022-02-08 13:19:11 -0800

    updates to side pande editor

--- Gruntfile.js
+++ Gruntfile.js
@@ -199,6 +199,7 @@ module.exports = function (grunt) {
           "packages/node_modules/@node-red/editor-client/src/js/ui/event-log.js",
           "packages/node_modules/@node-red/editor-client/src/js/ui/typeql.js",
           "packages/node_modules/@node-red/editor-client/src/js/ui/causeway.js",
+          "packages/node_modules/@node-red/editor-client/src/js/ui/causeway-outliner.js",
           "packages/node_modules/@node-red/editor-client/src/js/ui/tray.js",
           "packages/node_modules/@node-red/editor-client/src/js/ui/clipboard.js",
           "packages/node_modules/@node-red/editor-client/src/js/ui/library.js",
