eae4d0a3c582e02d4a3318c0b46c9ce9e891030e
yashua ovando <ovando.yashua@gmail.com>
2023-10-21 03:49:53 -0500

    fix local dev build and docker build issues

--- Gruntfile.js
+++ Gruntfile.js
@@ -708,9 +708,7 @@ module.exports = function (grunt) {
 
     grunt.registerTask('build',
         'Builds editor content',
-        ['clean:build', 'jsonlint', 'concat:build', 'concat:vendor', 'copy:build', 'uglify:build', 'sass:build']);
-
-        // ['clean:build', 'jsonlint', 'concat:build', 'concat:vendor', 'copy:build', 'uglify:build', 'sass:build', 'attachCopyright']);
+        ['clean:build', 'jsonlint', 'concat:build', 'concat:vendor', 'copy:build', 'uglify:build', 'sass:build', 'attachCopyright']);
 
     grunt.registerTask('build-dev',
         'Developer mode: build dev version',
